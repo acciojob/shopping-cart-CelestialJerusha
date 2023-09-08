fa9ada0339486e752a8ab8876906fce18b9793ec
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-09-08 22:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your code here
+var add = document.getElementById("add");
+add.addEventListener("click",add1);
+var total = document.getElementById("total");
+var sum = 0;
+
+function add1(){
+	var item = document.getElementById("item-name-input").value;
+	var price = document.getElementById("item-price-input").value;
+	var p = document.getElementById("para");
+	var tBody = document.getElementById("tb");
+	tb.innerHTML+=`
+			 <tr>
+				<td>${item}</td>
+				<td>${price}</td>
+			</tr>
+ `
+	sum+=parseInt(price);
+	total.innerHTML = "Total: "+sum;
+}
+total.innerHTML = "Total: "+sum;
+
+
+
+
+
+
